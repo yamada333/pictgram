0e4a2306365b4e8e1c15717bbe1c72e567cbe2b3
yamada333 <exvs7_fb7@yahoo.co.jp>
2022-04-25 19:19:13 +0900

    25

--- src/main/java/com/example/pictgram/form/TopicForm.java
+++ src/main/java/com/example/pictgram/form/TopicForm.java
@@ -1,5 +1,7 @@
 package com.example.pictgram.form;
 
+import java.util.List;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
@@ -16,6 +18,10 @@ public class TopicForm {
     private Long id;
 
     private Long userId;
+    
+    private List<FavoriteForm> favorites;
+
+    private FavoriteForm favorite;
 
     @ImageNotEmpty
     @ImageByte(max = 2000000)
